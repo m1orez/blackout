6ec1459dd189133f527138ac4bbbd8bf4882494a
M1orez <pauline@walhout-hia.nl>
2025-02-07 20:49:18 +0100

    game works code needs to be optimised

--- script/script.js
+++ script/script.js
@@ -2,6 +2,11 @@ let players = [];
 const addPlayer = document.getElementById('addPlayerBtn');
 const inputField = document.getElementById('playerName');
 const placeholders = document.querySelectorAll('.playerPlaceholder');
+const startGame = document.getElementById('startGame');
+const playerTurn = document.getElementById('playerTurn');
+const whiteCard = document.getElementById('whiteCard');
+const taskCard = document.getElementById('taskCard');
+const nextPlayer = document.getElementById('nextPlayer');
 
 addPlayer.addEventListener('click', () => {
     const playerName = inputField.value.trim();
@@ -27,3 +32,41 @@ function updatePlayerList() {
         placeholders[i].style.color = ""; 
     }
 }
+
+startGame.addEventListener('click', () => { 
+    if (players.length > 1) {
+        startGame.classList.add('active');
+    } else {
+        startGame.classList.remove('active');
+    }
+    document.getElementById('setUpGame').style.display = "none";
+    document.getElementById('playGame').style.display = "block";
+    playerTurn.innerHTML = `${players[0]}`;
+});
+
+const getRandomTask = () => {
+    fetch('../whiteCards.json')
+        .then(response => response.json())
+        .then(data => {
+            const tasks = data.tasks; 
+            const randomNumber = Math.floor(Math.random() * tasks.length);
+            const task = tasks[randomNumber].task;
+            taskCard.style.display = "block";
+            document.getElementById('randomTask').innerHTML = task;
+            whiteCard.style.display = "none";
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+};
+
+whiteCard.addEventListener('click', getRandomTask);
+
+nextPlayer.addEventListener('click', () => {
+    const currentPlayer = playerTurn.textContent;
+    const currentPlayerIndex = players.indexOf(currentPlayer);
+    const nextPlayerIndex = (currentPlayerIndex + 1) % players.length;
+    playerTurn.innerHTML = players[nextPlayerIndex];
+    whiteCard.style.display = "block";
+    taskCard.style.display = "none";
+});
